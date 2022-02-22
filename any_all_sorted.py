c9244d7c3cd7461dcb7f137daf9f86d4d2adf4b8
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-02-22 14:46:04 +0200

    Any, all, sorted

---
+++ any_all_sorted.py
@@ -0,0 +1,17 @@
+string = "HeallAobcDEB!"
+print(string)
+
+print(sorted(string))
+
+l1 = [True, False, False]
+l2 = [True, True]
+l3= []
+
+print(any(l1))
+print(any(l2))
+
+print(all(l1))
+print(all(l2))
+
+print(any(l3))
+print(all(l3))
